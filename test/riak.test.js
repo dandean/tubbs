6383e79eb8520e23ece76d4aa2129a1572c6363e
dandean <danwdean@gmail.com>
2011-07-20 10:57:50 -0700

    more work on the riak unit tests

--- test/riak.test.js
+++ test/riak.test.js
@@ -4,92 +4,89 @@ var Model = require('../index');
 
 var m;
 
+// TODO: complete these unit tests
+
 module.exports = {
   'riak': function() {
-    var TestModel, m;
+    var TestModel, m, id;
     
     createModelScaffold();
 
     function createModelScaffold() {
       TestModel = Model.create({
-        dataStore: new Model.RiakStore({ bucket: 'sites' }),
+        dataStore: new Model.RiakStore({ bucket: 'unit-tests' }),
         fields: {
           id: Guid.raw,
-          name: undefined,
-          owner: undefined,
-          theme: undefined
+          username: undefined
         }
       });
       
-      TestModel.all(function(e, results) {
-        console.log(e, results);
+      createAndSaveNewDocument();
+    }
+
+    function createAndSaveNewDocument() {
+      m = new TestModel({
+        username: "test 1"
+      });
+      
+      m.save(function(e, result) {
+        id = result.id;
+        assert.ok(Guid.isGuid(id));
+        getAllDocuments();
+      });
+    }
+    
+    function getAllDocuments() {
+      TestModel.all(function(e, documents) {
+        // documents.forEach(function(d) {
+        //   d.delete(function() {});
+        // });
+        assert.equal(1, documents.length);
+        assert.ok(documents[0] instanceof TestModel);
+        assert.equal(documents[0].username, 'test 1');
         
-        TestModel.find('60164656-6f7a-80b7-f9f1-5c74b8920809', function(e, results) {
-          console.log(e, results);
-          
-          TestModel.where(
-            function(doc) {
-              return doc.id == '60164656-6f7a-80b7-f9f1-5c74b8920809';
-            },
-            function(e, results) {
-              console.log(e, results);
-              
-              var record = results[0];
-              record.id = Guid.raw();
-              
-              record.save(function(e, result) {
-                console.log(e, result);
-                
-                record.delete(function(e, result) {
-                  console.log(e, result);
-                  
-                  TestModel.find(record.id, function(e, result) {
-                    console.log(e, result);
-                  });
-                });
-              });
-            }
-          );
+        m.delete(function(e, result) {
+          console.log(e, result);
         });
+        
       });
     }
+
+      
+      
+      
+      // TestModel.all(function(e, results) {
+      //   console.log(e, results);
+      //   
+      //   TestModel.find('60164656-6f7a-80b7-f9f1-5c74b8920809', function(e, results) {
+      //     console.log(e, results);
+      //     
+      //     TestModel.where(
+      //       function(doc) {
+      //         return doc.id == '60164656-6f7a-80b7-f9f1-5c74b8920809';
+      //       },
+      //       function(e, results) {
+      //         console.log(e, results);
+      //         
+      //         var record = results[0];
+      //         record.id = Guid.raw();
+      //         
+      //         record.save(function(e, result) {
+      //           console.log(e, result);
+      //           
+      //           record.delete(function(e, result) {
+      //             console.log(e, result);
+      //             
+      //             TestModel.find(record.id, function(e, result) {
+      //               console.log(e, result);
+      //             });
+      //           });
+      //         });
+      //       }
+      //     );
+      //   });
+      // });
     
-    function validateWithUsernameAsUndefined() {
-      m = new TestModel();
-      m.validate(function(e, success) {
-        assert.ok(e instanceof Error);
-        assert.ok('username' in m.errors);
-        assert.equal(1, m.errors.username.length);
-        validateWithUsernameAsNull();
-      });
-    }
-    
-    function validateWithUsernameAsNull() {
-      m.username = null;
-      m.validate(function(e, success) {
-        assert.ok(e instanceof Error);
-        assert.ok('username' in m.errors);
-        assert.equal(1, m.errors.username.length);
-        validateWithUsernameAsBlank();
-      });
-    }
-    
-    function validateWithUsernameAsBlank() {
-      m.username = '';
-      m.validate(function(e, success) {
-        assert.ok(e instanceof Error);
-        assert.ok('username' in m.errors);
-        assert.equal(1, m.errors.username.length);
-        validateWithUsernamePresent();
-      });
-    }
-    
-    function validateWithUsernamePresent() {
-      m.username = 'some username';
-      m.validate(function(e, success) {
-        assert.equal(null, e);
-        assert.ok('username' in m.errors === false);
-      });
-    }
+
   },
 };
